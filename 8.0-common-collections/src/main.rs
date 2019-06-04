ea0300da6501010159f11d0d5c5e5cfcf39bc1c9
Clay Boone <tener.hades@gmail.com>
2019-06-04 12:19:01 +0100

    making vectors

--- 8.0-common-collections/src/main.rs
+++ 8.0-common-collections/src/main.rs
@@ -1,11 +1,34 @@
-#![allow(unused_variables)]
-fn main() {
-    let v: Vec<i32> = Vec::new();
+#[derive(Debug)]
+struct Pets {
+    animal: String,
+    name: String,
+    age: i32,
+}
 
-    let mut v = vec![1, 1];
-    v.push(2);
-    v.push(3);
-    v.push(5);
+fn main() {
+    let p = vec![
+        Pets {
+            animal: String::from("Cat"),
+            name: String::from("Bob"),
+            age: 4,
+        },
+        Pets {
+            animal: String::from("Dog"),
+            name: String::from("Sudo"),
+            age: 10,
+        },
+    ];
+
+    println!("{:?}", p);
+
+    let mut v: Vec<u32> = Vec::new();
+    for _ in 0..20 {
+        match v.len() {
+            0 => v.push(0),
+            1 => v.push(1),
+            _ => v.push(v[v.len() - 1] + v[v.len() - 2]),
+        }
+    }
 
     println!("{:?}", v);
 }
